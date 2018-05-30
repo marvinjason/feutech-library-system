bf8b288b49bc7e01f7c475e4961ab744637ffd3a
marvinjason <marvinjason.sy@gmail.com>
2018-05-31 01:15:21 +0800

    Add pages controller, setup template rendering, and update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  private
+  def template
+    "#{controller_name}/#{action_name}/#{current_user.role}"
+  end
 end
