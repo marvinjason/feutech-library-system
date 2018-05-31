4f4e5ec83648fd8f6bfe06aef4151bc6992c3250
marvinjason <marvinjason.sy@gmail.com>
2018-05-31 22:19:10 +0800

    Add google login

--- app/controllers/auth/callbacks_controller.rb
+++ app/controllers/auth/callbacks_controller.rb
@@ -4,6 +4,11 @@ class Auth::CallbacksController < Devise::OmniauthCallbacksController
     sign_in_and_redirect user
   end
 
+  def google_oauth2
+    user = User.from_omniauth(auth_hash)
+    sign_in_and_redirect user
+  end
+
   def failure
     redirect_to :root
   end
