5d1e1e20259115a468f1bffe87419152adf4ec86
marvinjason <marvinjason.sy@gmail.com>
2018-06-01 02:45:55 +0800

    Update login layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  layout :resolve_layout
 
   private
+  def resolve_layout
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
+
   def template
     "#{controller_name}/#{action_name}/#{current_user.role}"
   end
