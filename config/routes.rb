9699a8b0df6f1b00e77961ee3bed7426f6ae61ac
marvinjason <marvinjason.sy@gmail.com>
2018-06-05 00:40:40 +0800

    Remove controllers and views for scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-
-  resources :comments
-  
-  resources :reviews
-  
-  resources :reservations
-  
-  resources :authors
-  
-  resources :books
-  
-  resources :publishers
-  
-  resources :users
-
   devise_for :users, controllers: {
     omniauth_callbacks: 'auth/callbacks'
   }
