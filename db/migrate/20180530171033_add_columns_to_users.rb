08098ca89ee04e71ae488cb475f7a96fb1e199c9
marvinjason <marvinjason.sy@gmail.com>
2018-06-04 22:47:55 +0800

    Add columns to user

--- db/migrate/20180530171033_add_columns_to_users.rb
+++ db/migrate/20180530171033_add_columns_to_users.rb
@@ -1,7 +1,12 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
+    add_column :users, :last_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :contact_number, :string
+    add_column :users, :address, :string
+    add_column :users, :role, :string, default: 'user'
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :role, :string, default: 'user'
+    add_column :users, :avatar, :string
   end
 end
