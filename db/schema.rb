21f7fde4504dc3fb244971f504bae8a7b9953b55
marvinjason <marvinjason.sy@gmail.com>
2018-06-05 23:38:10 +0800

    Update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604153100) do
+ActiveRecord::Schema.define(version: 20180604171459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180604153100) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authorships", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_authorships_on_author_id"
+    t.index ["book_id"], name: "index_authorships_on_book_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -110,6 +119,8 @@ ActiveRecord::Schema.define(version: 20180604153100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "authorships", "authors"
+  add_foreign_key "authorships", "books"
   add_foreign_key "books", "publishers"
   add_foreign_key "comments", "books"
   add_foreign_key "comments", "comments"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Publisher.create(
+    name: Faker::Book.publisher
+  )
+end
+
+5.times do
+  Author.create(
+    last_name: Faker::Name.last_name,
+    first_name: Faker::Name.first_name,
+    description: Faker::Lorem.paragraph,
+    remote_avatar_url: 'https://avatars3.githubusercontent.com/u/22198984?s=460&v=4'
+  )
+end
+
+EDITIONS = ['Book Club', 'First', 'Second', 'Third'].freeze
+
+25.times do
+  book = Book.create(
+    title: Faker::Book.title,
+    publisher: Publisher.all.sample,
+    publication_address: Faker::Address.country,
+    publication_year: (1980..2018).to_a.sample,
+    description: Faker::Lorem.paragraph,
+    edition: EDITIONS.sample,
+    isbn: '978-3-16-148410-0',
+    call_number: 'PR 8923 W6 L36 1990 c.3',
+    language: 'English',
+    page_count: 245,
+    book_format: 'Paperback',
+    remote_cover_url: 'https://images.gr-assets.com/books/1420911526l/3685.jpg',
+    total_count: 5
+  )
+
+  (1..2).to_a.sample.times do
+    book.authors << Author.all.sample
+  end
+end
