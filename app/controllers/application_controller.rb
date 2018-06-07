f06f24fa49fe18368c55738f41410c647be3995f
marvinjason <marvinjason.sy@gmail.com>
2018-06-07 21:03:22 +0800

    Update featured books layout & fix flash message and redirection when reserving book while not authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_current_location, unless: :devise_controller?
   layout :resolve_layout
 
+  protected
+  def after_sign_in_path_for(resource)
+    flash.discard
+    flash[:success] = 'You have succesfully signed in'
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
   private
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
   def resolve_layout
     if devise_controller?
       'devise'
