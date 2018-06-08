415dea93f8197e032ff825e39be6cf7b6184c5f2
marvinjason <marvinjason.sy@gmail.com>
2018-06-08 10:19:48 +0800

    Implement write a review toggle for form

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -2,6 +2,12 @@ var ready = function() {
   $('#reserve').on('click', function() {
     $('.tiny.modal').modal('show');
   });
+
+  $('#review-link').on('click', function(e) {
+    e.preventDefault();
+    $(this).hide();
+    $('#review-field').show();
+  });
 };
 
 // $(document).ready(ready);
