1bd818a657e00c3b6ffd188b6121188966f1827c
marvinjason <marvinjason.sy@gmail.com>
2018-06-13 20:21:34 +0800

    Add region for carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,6 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY'],
     aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: 'ap-southeast-1'
   }
 end
